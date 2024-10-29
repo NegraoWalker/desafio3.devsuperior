e1c765a7a8ec31daa13d25bf42aad19fcd0cd178
Walker Negrão <c0662240@vale.com>
2024-10-29 13:22:17 -0300

    Criação da classe de entidade Clients

---
+++ src/main/java/desafio3/devsuperior/controller/ClientController.java
@@ -0,0 +1,11 @@
+package desafio3.devsuperior.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+
+}
